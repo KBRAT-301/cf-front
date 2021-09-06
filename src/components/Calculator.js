7f75b288ce5017ecda00bf4cafb19f6606ee8035
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-09-06 16:41:25 -0700

    frontend skeleton

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+    render() {
+      return (
+        <div className="Calculator">
+          <p>Calculator</p>
+        </div>
+      );
+    }
+  }
+  export default Calculator;
