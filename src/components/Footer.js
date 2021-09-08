01c89928eb0d0438b1800c26ee71b7b4a9d78653
Ryan Emmans <ryanemmans@gmail.com>
2021-09-08 00:06:29 -0700

    Adds minor styling changes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>Created by team KBRAT</p>
+        <p>Created by Team KBRAT &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; &#169;Code Fellows 2021</p>
       </footer>
     );
   }
