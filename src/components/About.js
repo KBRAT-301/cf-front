1e424c07143dc842b3fc54aebaa176c716cae789
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-09-07 22:10:07 -0700

    bit o style to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,28 +4,44 @@ import { withAuth0 } from '@auth0/auth0-react';
 class About extends React.Component {
   render() {
     return (
-      <div>
+      <div style={{ backgroundImage: "url(/img/piano-background.jpg)" }}>
         <h2>About</h2>
-        <div>
+
+        <div style={{
+          display:'flex', 
+          justifyContent: 'flex-start' }} >
           <img src="https://via.placeholder.com/150" alt="Kyle Cohen" />
           <p>about kyle</p>
         </div>
-        <div>
+
+        <div style={{
+          display:'flex', 
+          justifyContent: 'flex-end' }}>
           <p>About Brandon</p>
           <img src="https://via.placeholder.com/150" alt="Brandon Grimes" />
         </div>
-        <div>
+
+        <div style={{
+          display:'flex', 
+          justifyContent: 'flex-start' }}>
           <img src="https://via.placeholder.com/150" alt="Ryan Emmans" />
           <p>About Ryan</p>
         </div>
-        <div>
+
+        <div style={{
+          display:'flex', 
+          justifyContent: 'flex-end'}}>
           <p>About Alex</p>
           <img src="https://via.placeholder.com/150" alt="Alex Grazda" />
         </div>
-        <div>
+        
+        <div style={{
+          display:'flex', 
+          justifyContent: 'flex-start' }}>
           <img src="https://via.placeholder.com/150" alt="Tray Chea" />
           <p>About Tray</p>
         </div>
+
       </div>
     );
   }
