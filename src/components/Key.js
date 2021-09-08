983989b73fc2ac946e144047014840ba17eafe64
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-09-07 22:46:21 -0400

    added keypress event handling

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,27 +1,35 @@
-// import _ from 'lodash';
 import React from 'react';
+
 import './Key.css';
-// import { NOTE_TO_KEY } from '../keyMap';
+import { NOTE_TO_KEY } from '../keyMap';
 
 export default class Key extends React.Component {
   noteIsFlat = (note) => {
     return note.includes('fl');
   }
 
+  keyIsPressed = (note, pressedKeys) => {
+    return pressedKeys.includes(NOTE_TO_KEY[note]);
+  }
+
   render() {
-    let keyClassName = 'key';
     const noteIsFlat = this.noteIsFlat(this.props.note);
+    const keyIsPressed = this.keyIsPressed(this.props.note, this.props.pressedKeys);
+    let key;
+    let keyClassName = 'key';
 
     if (noteIsFlat) {
       keyClassName += ' flat';
     }
-    let key;
+    if (keyIsPressed) {
+      keyClassName += ' pressed';
+    }
     if (noteIsFlat) {
       key = <div className={keyClassName}></div>;
     } else {
       key = (
         <div className={keyClassName}>
-          <div className="key-text">{this.props.note.toUpperCase()}</div>
+          <div className='key-text'>{this.props.note.toUpperCase()}</div>
         </div>
       );
     }
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,18 +1,58 @@
-import _ from 'lodash';
 import React from 'react';
+
 import './Keyboard.css';
 import Key from './Key';
 import {
   NOTES,
-  // VALID_KEYS,
-  // KEY_TO_NOTE,
+  VALID_KEYS,
+  KEY_TO_NOTE,
 } from '../keyMap';
 
 export default class Keyboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressedKeys: [],
+    };
+  }
+  handleKeyDown = (e) => {
+    if (!e.repeat) {
+      let key = e.key;
+      let newPressedKeys = [...this.state.pressedKeys];
+      if (!newPressedKeys.includes(key) && VALID_KEYS.includes(key)) {
+        newPressedKeys.push(key);
+      }
+      this.setState({ pressedKeys: newPressedKeys });
+      this.playNote(KEY_TO_NOTE[key]);
+    }
+  };
+
+  handleKeyUp = (e) => {
+    let key = e.key;
+    let index = this.state.pressedKeys.indexOf(e.key);
+    let newPressedKeys = [...this.state.pressedKeys];
+    if (newPressedKeys.includes(key) && VALID_KEYS.includes(key)) {
+      newPressedKeys.splice(index, 1);
+    }
+    this.setState({ pressedKeys: newPressedKeys });
+  };
+
+  playNote = (note) => {
+    // if (!note.isEmpty()) {
+    //   const noteAudio = new Audio(document.getElementById(note).src);
+    //   noteAudio.play();
+    // }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('keyup', this.handleKeyUp);
+  };
+
   render() {
-    const keys = _.map(NOTES, (note, idx) => {
+    const keys = NOTES.map((note, idx) => {
       return (
-        <Key key={idx} note={note} />
+        <Key key={idx} note={note} pressedKeys={this.state.pressedKeys}/>
       );
     });
 
@@ -21,5 +61,5 @@ export default class Keyboard extends React.Component {
         {keys}
       </div>
     );
-  }
+  };
 }
