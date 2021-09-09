a8e38c475369352c5e513e759a37e03e00c847df
Ryan Emmans <ryanemmans@gmail.com>
2021-09-08 19:34:55 -0700

    Adds content to about page, uses Semantic UI for styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,65 @@
 import React from 'react';
-import { Navbar, NavItem } from 'react-bootstrap';
+import { Menu, Segment, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { withAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton.js';
 import LogoutButton from './LogoutButton.js';
 
 class Header extends React.Component {
+  state = { activeItem: 'home' }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state;
+
     return (
       <header>
-        <Navbar>
-          <Navbar.Brand className="m-3">
-            <h1>Keys!</h1>
-          </Navbar.Brand>
-          <NavItem className="navItem">
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </NavItem>
-          <NavItem className="navItem">
-            <Link to="/profile" className="link">
-              Profile
-            </Link>
-          </NavItem>
-          <NavItem className="navItem">
-            <Link to="/about" className="link">
-              About
-            </Link>
-          </NavItem>
-          <NavItem className="navItem">
-            {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
-          </NavItem>
-        </Navbar>
+        <Segment inverted style={{ height: '100%' }}>
+          <Menu inverted pointing secondary style={{ justifyContent: 'center', marginTop: '-20px' }}>
+            <Menu.Item>
+              <h1 style={{ fontSize: '3rem' }}>Keys!</h1>
+            </Menu.Item>
+            <Menu.Item
+              link
+              className="navItem"
+              name='home'
+              size='big'
+              active={activeItem === 'home'}
+              onClick={this.handleItemClick}>
+              <Link to="/" style={{ textTransform: 'uppercase', fontWeight: 'bold' }} >
+                <Icon name='music' />
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item
+              link
+              className="navItem"
+              name='profile'
+              size='big'
+              active={activeItem === 'profile'}
+              onClick={this.handleItemClick}>
+              <Link to="/profile" style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+                <Icon name='child' />
+                Profile
+              </Link>
+            </Menu.Item>
+            <Menu.Item
+              link
+              className="navItem"
+              name='about'
+              size='big'
+              active={activeItem === 'about'}
+              onClick={this.handleItemClick}>
+              <Link to="/about" style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+                <Icon name='users' />
+                About
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            </Menu.Item>
+          </Menu>
+        </Segment>
       </header>
     );
   }
