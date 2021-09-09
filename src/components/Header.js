6d6f15fa6c6f345177cdc47083995ef08bec2772
Ryan Emmans <ryanemmans@gmail.com>
2021-09-09 08:12:27 -0700

    Adds keyboard rotation and nav wrap.

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ class Header extends React.Component {
     return (
       <header>
         <Segment inverted style={{ height: '100%' }}>
-          <Menu inverted pointing secondary style={{ justifyContent: 'center', marginTop: '-15px' }}>
+          <Menu inverted pointing secondary style={{ justifyContent: 'center', marginTop: '-15px', flexFlow: 'row wrap' }}>
             <Menu.Item>
               {/* <h1 style={{ fontSize: '3rem', marginBottom: '-5px'}}>Keys!</h1> */}
               <Image src={logo} alt="keys logo" style={{ height: '70px' }} />
